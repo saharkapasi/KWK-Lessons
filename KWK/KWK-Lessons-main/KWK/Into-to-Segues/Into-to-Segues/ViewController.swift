555b7a50cc60ab7e6ca33cb9a1d97e7641efa16a
saharkapasi <88101640+saharkapasi@users.noreply.github.com>
2021-07-29 12:08:40 -0400

    Intro to Segues Commit

---
+++ KWK/KWK-Lessons-main/KWK/Into-to-Segues/Into-to-Segues/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Into-to-Segues
+//
+//  Created by Nabil Kapasi on 7/29/21.
+//  Copyright © 2021 Sahar Kapasi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func submitButtonTapped(_ sender: Any) {
+    }
+    
+}
+
