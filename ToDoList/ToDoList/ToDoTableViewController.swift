d5e8dc91d88b4cc6653a22a0caa6c19ade333ad6
saharkapasi <88101640+saharkapasi@users.noreply.github.com>
2021-08-02 11:06:34 -0400

    Iteration 0

--- ToDoList/ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -9,14 +9,24 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
 
+    var listOfToDo : [ToDoClass] = []
+    
+    func createToDo () -> [ToDoClass] {
+        let swiftToDo = ToDoClass()
+        swiftToDo.description = "Learn Swift"
+        swiftToDo.important = true
+        
+        let dogToDo = ToDoClass ()
+        dogToDo.description = "Walk the Dog"
+        //important set to false by default
+        
+        return [swiftToDo, dogToDo]
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+     listOfToDo = createToDo()
     }
 
     // MARK: - Table view data source
@@ -28,19 +38,24 @@ class ToDoTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 2
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let eachToDo = listOfToDo[indexPath.row]
+        cell.textLabel?.text = eachToDo.description
+        
+        if eachToDo.important {
+            cell.textLabel?.text = "❗️" + eachToDo.description
+        } else {
+            cell.textLabel?.text = eachToDo.description
+        }
 
         return cell
     }
-    */
-
+  
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
