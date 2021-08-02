0ab6d3525fba4309bf2f763d52b4bfd0878c0429
saharkapasi <88101640+saharkapasi@users.noreply.github.com>
2021-08-02 12:17:29 -0400

    Working on ToDoList Project Iteration 1

--- ToDoList/ToDoList/ToDoTableViewController.swift
+++ ToDoList/ToDoList/ToDoTableViewController.swift
@@ -91,14 +91,13 @@ class ToDoTableViewController: UITableViewController {
     }
     */
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let nextAddToDoVC = segue.destination as? AddToDoViewController {
+            nextAddToDoVC.previousToDoTVC = self
     }
-    */
-
+}
 }
