e4bcbb9cac1f63a8bcbc4c00f0e876abffb2363e
saharkapasi <88101640+saharkapasi@users.noreply.github.com>
2021-08-03 10:25:15 -0400

    Completed Iteration 1

--- ToDoList/ToDoList/CompletedToDoViewController.swift
+++ ToDoList/ToDoList/CompletedToDoViewController.swift
@@ -2,17 +2,22 @@
 //  CompletedToDoViewController.swift
 //  ToDoList
 //
-//  Created by Nabil Kapasi on 8/3/21.
+//  Created by Sahar Kapasi on 8/3/21.
 //
 
 import UIKit
 
 class CompletedToDoViewController: UIViewController {
+    
+    var previousToDoTVC = ToDoTableViewController()
+    var selectedToDo = ToDoClass()
 
     @IBOutlet weak var toDoDisplay: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        toDoDisplay.text = selectedToDo.description
 
         // Do any additional setup after loading the view.
     }
